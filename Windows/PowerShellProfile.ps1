e0ea71cac297514e0f043af9e404894528ef2cd0
gzevieré <99291357+gzeviere@users.noreply.github.com>
2022-10-17 18:33:51 +0200

    Update PowerShellProfile.ps1

--- Windows/PowerShellProfile.ps1
+++ Windows/PowerShellProfile.ps1
@@ -1,5 +1,5 @@
 Import-Module -Name Terminal-Icons
-oh-my-posh init pwsh --config 'C:\Users\antoi\Documents\.config\peru.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\antoi\.config\peru.omp.json' | Invoke-Expression
 
 #Aliases
 Set-Alias touch -Value New-Item
