119cd8889e12cd928ef34f08959b39c662a5fc50
gzavier <gzavier@protonmail.com>
2022-10-17 18:26:16 +0200

    win pwsh conf added

---
+++ Windows/PowerShellProfile.ps1
@@ -0,0 +1,8 @@
+Import-Module -Name Terminal-Icons
+oh-my-posh init pwsh --config 'C:\Users\antoi\Documents\.config\peru.omp.json' | Invoke-Expression
+
+#Aliases
+Set-Alias touch -Value New-Item
+
+function GOBACK {set-location ../.}
+Set-Alias .. -Value GOBACK
