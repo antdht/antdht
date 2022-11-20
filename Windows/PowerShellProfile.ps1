15e534cea1bc719dcf210067d3fd940871407386
gzevieré <99291357+gzeviere@users.noreply.github.com>
2022-11-20 19:26:01 +0100

    Update PowerShellProfile.ps1

--- Windows/PowerShellProfile.ps1
+++ Windows/PowerShellProfile.ps1
@@ -1,6 +1,10 @@
 Import-Module -Name Terminal-Icons
 oh-my-posh init pwsh --config 'C:\Users\antoi\.config\peru.omp.json' | Invoke-Expression
 
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
 #Aliases
 Set-Alias touch -Value New-Item
 
